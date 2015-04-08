2529850f4e4afbf0e7b7c9983c5341a7c6c2d849
Félix Saparelli <felix.saparelli@mckay.co.nz>
2015-04-09 09:18:11 +1200

    Initial commit

---
+++ src/Util.php
@@ -0,0 +1,23 @@
+<?
+
+namespace McKay;
+
+class Util {
+	public static function time($datetime = null, $classes = '') {
+		if ($datetime === null) { $datetime = time(); }
+		return sprintf('<time datetime="%1$s" class="%2$s">%1$s</time>',
+			date(DateTime::RFC3339, $datetime),
+			trim('render-time ' . $classes));
+	}
+
+	public static function slug($string, $delimiter = '-') {
+		static $slugifier = null;
+		if ($slugifier === null) {
+			$slugifier = new \Cocur\Slugify\Slugify();
+		}
+
+		return $slugifier->slugify($string, $delimiter);
+	}
+}
+
+?>
